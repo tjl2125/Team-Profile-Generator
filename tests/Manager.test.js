a139fcba1eb5c044eb028a46505ed6178c893633
Taylor Leong <tjl2125@alum.barnard.edu>
2021-04-09 01:21:25 -0400

    finished setting up manager test

---
+++ tests/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    describe ("Initialization", () => {
+        it ("can set the office number through the constructor argument")
+        const offNum = 123; 
+        const manager = new Manager("Man", 1, "man@example.com", offNum);
+        expect(manager.officeNum).toBe(offNum); 
+    })
+    describe ("getRole()", () => {
+        it ("should return Manager")
+        const role = "Manager";
+        const manager = new Manager ("Man",1,"man@example.com",123); 
+        expect(manager.getRole()).toBe(role);
+    })
+    describe ("getOffice()", () => {
+        it ("can get the office number through getOffice()")
+        const oNum = 123; 
+        const manager = new Manager ("Man",1,"man@example.com",oNum);
+        expect(manager.getOfficeNum()).toBe(oNum); 
+    })
+})
