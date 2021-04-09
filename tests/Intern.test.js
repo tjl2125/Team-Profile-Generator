fca8d3a97227445add6f25b3084f29dbb8d794ce
Taylor Leong <tjl2125@alum.barnard.edu>
2021-04-09 01:50:22 -0400

    fixed intern test maybe

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,24 +1,35 @@
 const Intern = require ("../lib/Intern"); 
 
-describe("Intern", () => {
-    describe("Initialization", ()=> {
-        it ("can set the university through the constructor" )
-        const uni = "Columbia University";
-        const intern = new Intern ("Int", 1, "student@example.com", uni);
-        expect (intern.university).toBe(uni); 
-    });
-
-    describe("getRole", () => {
-        it ("getRole() should return Intern")
-        const role = "Intern"; 
-        const intern = new Intern ("Int", 1, "student@example.com", "Columbia University");
-        expect(intern.getRole()).toBe(role); 
-    });
-
-    describe("getUniversity()", () => {
-        it ("getUniveristy() can get the university")
-        const univ = "Columbia University";
-        const intern = new Intern("Int",1,"student@example.com",univ);
-        expect(intern.getUniversity()).toBe(univ); 
-    });
+describe ("Intern", () => {
+    describe ("Initialization", () => {
+        it ("returns the Intern role through getRole()", () => {
+            const intern = new Intern (); 
+            expect (intern.getRole()).toBe("Intern"); 
+        })
+        it ("returns the name of the university through getUniversity()", () => {
+            const intern = new Intern (); 
+            const uni = "Columbia"; 
+            expect (intern.getUniversity()).toBe("Columbia"); 
+        })
+    })
 })
+
+
+
+    test ("can set the university through the constructor", ()=> { 
+    const uni = "Columbia University";
+    const intern = new Intern ("Int", 1, "student@example.com", uni);
+    expect (intern.university).toBe(uni); 
+    });
+
+    test ("getRole() should return Intern", () => {
+    const role = "Intern"; 
+    const intern = new Intern ("Int", 1, "student@example.com", "Columbia University");
+    expect(intern.getRole()).toBe(role); 
+    });
+
+    test ("getUniveristy() can get the university name", () => {
+    const univ = "Columbia University";
+    const intern = new Intern("Int",1,"student@example.com",univ);
+    expect(intern.getUniversity()).toBe(univ); 
+    });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -2,19 +2,21 @@ const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
     describe ("Initialization", () => {
-        it ("can set the office number through the constructor argument")
+        test ("can set the office number through the constructor argument")
         const offNum = 123; 
         const manager = new Manager("Man", 1, "man@example.com", offNum);
         expect(manager.officeNum).toBe(offNum); 
     })
+
     describe ("getRole()", () => {
-        it ("should return Manager")
+        test ("should return Manager")
         const role = "Manager";
         const manager = new Manager ("Man",1,"man@example.com",123); 
         expect(manager.getRole()).toBe(role);
     })
+
     describe ("getOffice()", () => {
-        it ("can get the office number through getOffice()")
+        test ("can get the office number through getOffice()")
         const oNum = 123; 
         const manager = new Manager ("Man",1,"man@example.com",oNum);
         expect(manager.getOfficeNum()).toBe(oNum); 
